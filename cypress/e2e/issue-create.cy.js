8b99689f8b829e7fcecec697d22f0274cda70a1c
KohtT <tanel@transify.ee>
2023-07-04 13:48:58 +0300

    Add Issue Creation Tests

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,6 @@
+import { faker } from '@faker-js/faker';
+
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -11,10 +14,6 @@ describe('Issue create', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
       
-      //open issue type dropdown and choose Story
-      cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]')
-          .trigger('click');
             
       //Type value to description input field
       cy.get('.ql-editor').type('TEST_DESCRIPTION');
@@ -23,6 +22,11 @@ describe('Issue create', () => {
       //Order of filling in the fields is first description, then title on purpose
       //Otherwise filling title first sometimes doesn't work due to web page implementation
       cy.get('input[name="title"]').type('TEST_TITLE');
+
+       //open issue type dropdown and choose Story
+       cy.get('[data-testid="select:type"]').click();
+       cy.get('[data-testid="select-option:Story"]')
+         .trigger('click');
       
       //Select Lord Gaben from reporter dropdown
       cy.get('[data-testid="select:userIds"]').click();
@@ -54,6 +58,111 @@ describe('Issue create', () => {
       cy.get('[data-testid="icon:story"]').should('be.visible');
     });
   });
+
+  it('TEST1-Should create an issue and validate it successfully', () => {
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+
+
+      //Type value to description input field
+      cy.get('.ql-editor').type('My bug description');
+
+      //Type value to title input field
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      cy.get('input[name="title"]').type('Bug');
+
+       //open issue type dropdown and choose Bug
+       cy.get('[data-testid="select:type"]').click();
+       cy.get('[data-testid="select-option:Bug"]')
+         .trigger('click');
+ 
+
+      //Select Lord Gaben from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+      //Select Highest priority from Priority dropdown
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Highest"]').click();
+
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains('Bug');
+
+      cy.get('[data-testid="icon:bug"]').should('be.visible');
+
+
+    });
+  });
+
+  it('TEST2-Should create an issue and validate it successfully', () => {
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+      
+  
+            
+      //Type value to description input field using random data plugin
+      const randomWords = faker.lorem.words(3);
+      cy.get('.ql-editor').type(randomWords);
+
+      //Type randomWord to title input field using random data plugin
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      const randomWord = faker.lorem.words(1);
+      cy.get('input[name="title"]').type(randomWord);
+      
+      //Select Lord Gaben from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+      //Select Low priority from Priority dropdown
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+
+
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+    
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+          .should('have.length', '5')
+         
+      //Assert that type icon is visible
+      cy.get('[data-testid="icon:story"]').should('be.visible');
+    });
+  });
+
 
   it('Should validate title is required field if missing', () => {
     //System finds modal for creating issue and does next steps inside of it
